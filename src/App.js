11a7680558c56e2d44fecc318f40ee4547ad1251
dusenko-godaddy <dusenko@godaddy.com>
2022-05-11 21:42:17 +0300

    some commit

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import "./lib/collect/bundle";
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -26,7 +27,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   return (
